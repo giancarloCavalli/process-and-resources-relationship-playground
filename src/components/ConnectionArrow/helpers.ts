589aa6b4dd477863fb181c7891d3cd2884424507
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-06-16 17:21:36 -0300

    'feat: add arrow head in lines'

--- src/components/ConnectionArrow/helpers.ts
+++ src/components/ConnectionArrow/helpers.ts
@@ -4,10 +4,10 @@ export const getCurvedArrowPath = (fromPosition: Position, toPosition: Position,
   //https://www.beyondjava.net/how-to-connect-html-elements-with-an-arrow-using-svg
   //https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Higher-order_curves
   const x1 = fromPosition.left
-  const y1 = fromPosition.top - 40 + deviation
+  const y1 = fromPosition.top - 50 + deviation
 
-  const x2 = toPosition.left
-  const y2 = toPosition.top - 40 + deviation
+  const x2 = toPosition.left + getConnectionAdjustmentInXAxis(x1, toPosition.left)
+  const y2 = toPosition.top - 50 + deviation
 
   const delta = (x2 - x1) * slackness
 
@@ -18,4 +18,8 @@ export const getCurvedArrowPath = (fromPosition: Position, toPosition: Position,
   const hy2 = y2;
 
   return `M ${x1} ${y1} C ${hx1} ${hy1} ${hx2} ${hy2} ${x2} ${y2}`
+}
+
+const getConnectionAdjustmentInXAxis = (xFrom: number, xTo: number): number => {
+  return (xFrom - xTo >= 50 ? 70 : -15)
 }
--- src/components/ConnectionArrow/index.tsx
+++ src/components/ConnectionArrow/index.tsx
@@ -11,7 +11,27 @@ export const ConnectionArrow = ({ connections }: Props) => {
   return (
     <Wrapper height="100vh" width="100%">
       {connections.map(({ positionFrom, positionTo, lineSlackness, deviation }, index) => (
-        <path key={index} d={getCurvedArrowPath(positionFrom, positionTo, lineSlackness ?? 0, deviation ?? 0)} fill="none" stroke="red"></path>
+        <>
+          <defs>
+            <marker id="triangle" viewBox="0 0 10 10" refX="0" refY="5"
+              markerUnits="strokeWidth" markerWidth="17.5"
+              markerHeight="14" orient="auto"
+              style={{ zIndex: 1 }}
+            >
+              <path d="M 0 0 L 10 5 L 0 10 z"></path>
+            </marker>
+          </defs>
+          <path
+            key={index}
+            d={getCurvedArrowPath(positionFrom, positionTo, lineSlackness ?? 0, deviation ?? 0)}
+            fill="none"
+            stroke="red"
+            marker-end="url(#triangle)"
+            style={{ zIndex: -1 }}
+          >
+
+          </path>
+        </>
       ))}
     </Wrapper>
   )
