0e07ed14e08faabaf62834f2a6a383edda563be7
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-08-01 21:28:20 -0300

    test: add Button component tests

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render } from "@testing-library/react"
 
 import { Button } from './index'
 
@@ -8,5 +8,30 @@ describe('Button tests', () => {
     const { getByText } = render(<Button handleClick={() => { }}>Click me</Button>)
 
     expect(getByText(/Click/).textContent).toBe("Click me")
+  })
+
+  it('should render orange as default background color', () => {
+    const { getByRole } = render(<Button handleClick={() => { }}>Click me</Button>)
+
+    expect(getByRole("button")).toHaveStyle({
+      backgroundColor: 'orange'
+    })
+  })
+
+  it('should render background color passed as prop', () => {
+    const { getByRole } = render(<Button backgroundColor='green' handleClick={() => { }}>Click me</Button>)
+
+    expect(getByRole('button')).toHaveStyle({
+      backgroundColor: 'green'
+    })
+  })
+
+  it('should invoke onClick function on click', () => {
+    const onClickMock = jest.fn()
+    const { getByRole } = render(<Button handleClick={onClickMock}>Click me</Button>)
+
+    fireEvent.click(getByRole('button'))
+
+    expect(onClickMock).toHaveBeenCalledTimes(1)
   })
 })
