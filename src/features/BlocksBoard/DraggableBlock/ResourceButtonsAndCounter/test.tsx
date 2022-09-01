5b176755d482731e17eabb8b755fc0af4be4f16b
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-08-31 21:56:31 -0300

    test(component): add tests to ResourceButtonsAndCounter

--- src/features/BlocksBoard/DraggableBlock/ResourceButtonsAndCounter/test.tsx
+++ src/features/BlocksBoard/DraggableBlock/ResourceButtonsAndCounter/test.tsx
@@ -1,22 +1,51 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render } from "@testing-library/react"
 import { ResourceButtonsAndCounter } from "."
 
 describe('ResourceButtonsAndCounter tests', () => {
 
   it('should render two small buttons and 3 resource spheres', () => {
-    const onIncrementResourceQuantityMock = jest.fn()
-    const onDecrementResourceQuantity = jest.fn()
-
     const { getAllByRole, getAllByTestId } = render(
       <ResourceButtonsAndCounter
         resourceQuantity={3}
-        onIncrementResourceQuantity={onIncrementResourceQuantityMock}
-        onDecrementResourceQuantity={onDecrementResourceQuantity}
+        onIncrementResourceQuantity={jest.fn()}
+        onDecrementResourceQuantity={jest.fn()}
       />
     )
 
     expect(getAllByRole('button').length).toBe(2)
 
     expect(getAllByTestId('resource-sphere').length).toBe(3)
+  })
+
+  it('should call onIncrementResourceQuantity when clicking + button', () => {
+    const onIncrementResourceQuantityMock = jest.fn()
+
+    const { getByText } = render(
+      <ResourceButtonsAndCounter
+        resourceQuantity={1}
+        onIncrementResourceQuantity={onIncrementResourceQuantityMock}
+        onDecrementResourceQuantity={jest.fn()}
+      />
+    )
+
+    fireEvent.click(getByText('+'))
+
+    expect(onIncrementResourceQuantityMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call onDecrementResourceQuantity when clicking - button', () => {
+    const onDecrementResourceQuantity = jest.fn()
+
+    const { getByText } = render(
+      <ResourceButtonsAndCounter
+        resourceQuantity={1}
+        onIncrementResourceQuantity={jest.fn()}
+        onDecrementResourceQuantity={onDecrementResourceQuantity}
+      />
+    )
+
+    fireEvent.click(getByText('-'))
+
+    expect(onDecrementResourceQuantity).toHaveBeenCalledTimes(1)
   })
 })
